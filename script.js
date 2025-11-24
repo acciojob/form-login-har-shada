3feae92ade15ab1d9daadc551fdb3ca81dc7181d
har-shada <harshadapohane14@gmail.com>
2025-11-24 18:50:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
 
+	const firstName= document.querySelector('input[name="fname"]').value;
+	const lastName= document.querySelector('input[name="lname"]').value;
+		alert(`${firstName}  ${lastName}`);
+
+	
 }
+const button= document.querySelector('input[type="submit"]');
+button.addEventListener("click",(e)=>{
+
+e.preventDefault();
+getFormvalue();
+});
+
+
+
+getFormvalue();
